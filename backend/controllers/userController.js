2912f898fc5fa32f42f604bce8be48d0de64c24b
Mackowi <dzierzenga.maciej@gmail.com>
2023-07-11 15:44:26 +0200

    basic setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const asyncHandler = require('express-async-handler')
+
+// desc: Register new user
+// route: /api/users
+// access: public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please include all fields')
+  }
+  res.send('Register Route')
+})
+
+// desc: Login a user
+// route: /api/users/login
+// access: public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send('Login route')
+})
+
+module.exports = {
+  registerUser,
+  loginUser,
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const PORT = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const userRoutes = require('./routes/userRoutes')
+const connectDB = require('./config/db')
+
+// connect to db
+connectDB()
+
+const app = express()
+
+//json and urlencoded parsers
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the support' })
+})
+
+app.use('/api/users', userRoutes)
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
