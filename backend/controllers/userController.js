6bac4c8c3d521e3acbf4e02e111253d5473e232d
Mackowi <dzierzenga.maciej@gmail.com>
2023-07-12 14:41:52 +0200

    ticket routes, model, controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 
 // desc: Register new user
-// route: /api/users
+// route: POST /api/users
 // access: public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
@@ -49,7 +49,7 @@ const registerUser = asyncHandler(async (req, res) => {
 })
 
 // desc: Login a user
-// route: /api/users/login
+// route: POST /api/users/login
 // access: public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
@@ -72,7 +72,7 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 // desc: Get current user
-// route: /api/users/me
+// route: GET /api/users/me
 // access: private
 const getMe = asyncHandler(async (req, res) => {
   const user = {
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const colors = require('colors')
 const PORT = process.env.PORT || 5000
 const { errorHandler } = require('./middleware/errorMiddleware')
 const userRoutes = require('./routes/userRoutes')
+const ticketRoutes = require('./routes/ticketRoutes')
 const connectDB = require('./config/db')
 
 // connect to db
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/users', userRoutes)
+app.use('/api/tickets', ticketRoutes)
 
 app.use(errorHandler)
 
