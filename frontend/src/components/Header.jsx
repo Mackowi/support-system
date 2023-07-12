baaae1bfde608608fed9c95558b6cf3dfd812063
Mackowi <dzierzenga.maciej@gmail.com>
2023-07-12 10:50:47 +0200

    register, login page.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'>Support Desk</Link>
+        <Link to='/'>Support System</Link>
       </div>
       <ul>
         <li>
