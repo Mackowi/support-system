f71c54040675dc3fafafb5c774c355838e42e896
Mackowi <dzierzenga.maciej@gmail.com>
2023-07-12 18:33:31 +0200

    typo

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,9 +1,9 @@
 import { Navigate, Outlet } from 'react-router-dom'
-import { userAuthStatus } from '../hooks/useAuthStatus'
+import { UserAuthStatus } from '../hooks/UserAuthStatus'
 import Spinner from './Spinner'
 
 const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = userAuthStatus()
+  const { loggedIn, checkingStatus } = UserAuthStatus()
 
   if (checkingStatus) {
     return <Spinner />
--- frontend/src/hooks/UserAuthStatus.js
+++ frontend/src/hooks/UserAuthStatus.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
-export const useAuthStatus = () => {
+export const UserAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false)
   const [checkingStatus, setCheckingStatus] = useState(true)
 
